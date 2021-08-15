331aaa229ff9fa59c29cac5b0dc9b68c89d19a4a
usaid <usamasaid321@gmail.com>
2021-08-15 15:45:06 -0400

    This is my first revision

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index2.html')
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/cases')
+def cases():
+    return render_template('cases.html')
 
 
 if __name__ == '__main__':
