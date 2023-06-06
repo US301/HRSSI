a87f08ffb1091dd860f675fcf8e2539f388a5fbb
US301 <46348698+US301@users.noreply.github.com>
2023-06-06 15:12:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'usamasaid321@gmail.com'
-app.config['MAIL_PASSWORD'] = '301856Us12!'
+app.config['MAIL_PASSWORD'] = 'XXXXXXXXXXXXXXXXXX'
 
 mail = Mail(app)
 
